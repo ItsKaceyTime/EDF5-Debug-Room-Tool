8fa958e9c5870b2f5002d2056c35688c35a4f4d0
kacey <kaceystream@gmail.com>
2022-10-25 18:58:02 -0500

    first functional version

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 # EDF5-Debug-Room-Tool
-a gui to make lightweight changes to my earth defense force 5 debug room without the user having to learn and change the code themselves.
+a gui to make lightweight changes to my earth defense force 5 debug room without the user having to learn (and change) the code themselves.
 
 edf5 mission modding, currently, requires a solid amount of understanding and effort. my goal is to both create a tool to broaden the usage of a debug room i've created for an average user, as well as lower the barrier to entry, hopefully encouraging more people to look deeper into modding the game. i'm also hoping to be setting the groundwork for more user friendly modding tools.
 
 
-important notes about the current state of this repo:
 
-my debug room isn't currently released, i intend to release it alongside this tool.
+HOW TO USE:
+
+install BlueAmulet's ModLoader: https://github.com/BlueAmulet/EDFModLoader
+
+download Kittopia's Unnamed EDF Tools: https://gitlab.com/kittopiacreator/edf-tools
+
+after downloading, find EDF Tools.exe, located in the Release folder and copy it to my tool's directory
+
+launch run.bat
+
+select the enemies you wish to be spawnable in the map, and their variant if applicable
+
+click Confirm
+
+move the newly created MISSION.Bvm file into the included Mods/Mission/M003 folder
+
+copy the Mods folder into your root EDF5 directory, load into offline mission 3
+
+there are four shooting targets with functions upon being destroyed. 3 of them are tied to spawning the enemies you selected, with one tied to completing the mission
+
+enjoy!
+
+
+
+important notes about the current state of this repo:
 
 the first releasable state of the tool is going to focus on enemy spawns.
 
--- debugroomtool.py
+++ debugroomtool.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from subprocess import Popen, PIPE
 window = tk.Tk()
 window.title("Debug Room")
 #window.minsize(720,800)
@@ -12,17 +13,17 @@ labeltext = [
 ]
 
 enemylist = {
-    "ant" : "ANTFILEPATH",
+    "ant" : "app:/OBJECT/GIANTANT01.SGO",
     "frog" : { 
-        "basefrog" : {"filepath" : "FROGFILEPATH"},
-        "armor" : {"filepath" : "FROGARMORFILEPATH"},
-        "leader" : {"filepath" : "FROGLEADERFILEPATH"}
+        "basefrog" : {"filepath" : "app:/OBJECT/E503_FROG_AF.SGO"},
+        "armor" : {"filepath" : "app:/OBJECT/E503_ARMORFROG_AF.SGO"},
+        "leader" : {"filepath" : "app:/OBJECT/E503_FROG_AF_LEADER.SGO"}
     },        
-    "spider" : "SPIDERFILEPATH",
+    "spider" : "app:/OBJECT/GIANTSPIDER01.SGO",
     "alien" : {
-        "basealien" : {"filepath" : "ALIENFILEPATH"},
-        "armor" : {"filepath" : "ALIENARMORPATH"},
-        "leader" : {"filepath" : "ALIENLEADERPATH"}
+        "basealien" : {"filepath" : "app:/OBJECT/BIGGREY_AF.SGO"},
+        "LL?" : {"filepath" : "app:/OBJECT/BIGGREY_LL.SGO"},
+        "leader" : {"filepath" : "app:/OBJECT/BIGGREY_AF_LEADER.SGO"}
     }
 }
 
@@ -40,7 +41,7 @@ def callback(i,r):
         drop2.set("")
         drop2['state'] = "disabled"
         path.config(text = enemylist[drop.get()])
-        path.grid(row=r, column=1, padx=10, pady=10)
+        path.grid(row=r, column=1, padx=10, pady=10, sticky="nsew")
 
 def filepath(i,r):
     drop = variables[i]
@@ -55,34 +56,34 @@ mainframe = tk.Frame(master=window, relief=tk.GROOVE, borderwidth=2)
 mainframe.grid()
 
 enemy1frame = tk.Frame(master=mainframe, relief=tk.GROOVE, borderwidth=2)
-enemy1frame.grid(row=0,column=0)
+enemy1frame.grid(row=0,column=0, sticky="nsew")
 enemy1label = tk.Label(master=enemy1frame, text=labeltext[0])
 enemy1label.grid(row=0, column=0, pady=10)
 
 enemy2frame = tk.Frame(master=mainframe, relief=tk.GROOVE, borderwidth=2)
-enemy2frame.grid(row=2, column=0)
+enemy2frame.grid(row=2, column=0, sticky="nsew")
 enemy2label = tk.Label(master=enemy2frame, text=labeltext[1])
 enemy2label.grid(row=2, column=0, pady=10)
 
 enemy3frame = tk.Frame(master=mainframe, relief=tk.GROOVE, borderwidth=2)
-enemy3frame.grid(row=4, column=0)
+enemy3frame.grid(row=4, column=0, sticky="nsew")
 enemy3label = tk.Label(master=enemy3frame, text=labeltext[2])
 enemy3label.grid(row=4, column=0, pady=10)
 
 enemydrop1a = ttk.Combobox(master=enemy1frame, values=list(enemylist.keys()), state="readonly")
 enemydrop1a.current(0)
 enemydrop1b = ttk.Combobox(master=enemy1frame, values=[], state="disabled")
-enemy1path = tk.Label(master=enemy1frame, text="ANTFILEPATH")
+enemy1path = tk.Label(master=enemy1frame, text="app:/OBJECT/GIANTANT01.SGO")
 
 enemydrop2a = ttk.Combobox(master=enemy2frame, values=list(enemylist.keys()), state="readonly")
 enemydrop2a.current(0)
 enemydrop2b = ttk.Combobox(master=enemy2frame, values=[], state="disabled")
-enemy2path = tk.Label(master=enemy2frame, text="ANTFILEPATH")
+enemy2path = tk.Label(master=enemy2frame, text="app:/OBJECT/GIANTANT01.SGO")
 
 enemydrop3a = ttk.Combobox(master=enemy3frame, values=list(enemylist.keys()), state="readonly")
 enemydrop3a.current(0)
 enemydrop3b = ttk.Combobox(master=enemy3frame, values=[], state="disabled")
-enemy3path = tk.Label(master=enemy3frame, text="ANTFILEPATH")
+enemy3path = tk.Label(master=enemy3frame, text="app:/OBJECT/GIANTANT01.SGO")
 
 variables = [
     enemydrop1a,
@@ -105,15 +106,15 @@ enemydrop2b.bind('<<ComboboxSelected>>', lambda i : filepath(3,2))
 enemydrop3a.bind('<<ComboboxSelected>>', lambda i : callback(6,4))
 enemydrop3b.bind('<<ComboboxSelected>>', lambda i : filepath(6,4))
 
-enemydrop1a.grid(row=1, column=0, padx=10, pady=10)
-enemydrop1b.grid(row=1, column=1, padx=10, pady=10)
-enemy1path.grid(row=0, column=1, padx=10, pady=10)
-enemydrop2a.grid(row=3, column=0, padx=10, pady=10)
-enemydrop2b.grid(row=3, column=1, padx=10, pady=10)
-enemy2path.grid(row=2, column=1, padx=10, pady=10)
-enemydrop3a.grid(row=5, column=0, padx=10, pady=10)
-enemydrop3b.grid(row=5, column=1, padx=10, pady=10)
-enemy3path.grid(row=4, column=1, padx=10, pady=10)
+enemydrop1a.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+enemydrop1b.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+enemy1path.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+enemydrop2a.grid(row=3, column=0, padx=10, pady=10, sticky="nsew")
+enemydrop2b.grid(row=3, column=1, padx=10, pady=10, sticky="nsew")
+enemy2path.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+enemydrop3a.grid(row=5, column=0, padx=10, pady=10, sticky="nsew")
+enemydrop3b.grid(row=5, column=1, padx=10, pady=10, sticky="nsew")
+enemy3path.grid(row=4, column=1, padx=10, pady=10, sticky="nsew")
 
 def button():
 
@@ -123,7 +124,7 @@ def button():
     "%SPAWN1AMOUNTINT%" : "1",
     "%SPAWN1HEALTHFLOAT%" : "1.0f",
     "%SPAWN1AGGROBOOL%" : "1",
-    
+
     "%SPAWN2RADIUSFLOAT%" : "1.0f",
     "%SPAWN2ENEMYSTRING%" : enemy2path['text'],
     "%SPAWN2AMOUNTINT%" : "1",
@@ -139,21 +140,29 @@ def button():
     
     global window2
     window2 = tk.Toplevel(master=mainframe)
-    window2.title = "Inject"
+    window2.title = "Finished"
     window2.resizable(width=False, height=False)
+    x = window.winfo_x()
+    y = window.winfo_y()
+    window2.geometry("+%d+%d" %(x+200,y+200))
+    window2.wm_transient(window)
     window2.grid()
-    popuplabel = tk.Label(master=window2, text="Injected! (Not really yet lol)")
-    closebutton = tk.Button(master=window2, text="Close", relief=tk.GROOVE, borderwidth=5, command=popupdestroy)
+    popupframe = tk.Frame(master=window2)
+    popuplabel = tk.Label(master=popupframe, text="Finished! Check the .exe's folder.")
+    closebutton = tk.Button(master=popupframe, text="Close", relief=tk.GROOVE, borderwidth=5, command=popupdestroy)
     confirm['state'] = "disabled"
-    popuplabel.grid()
-    closebutton.grid(sticky="nsew")
+    popupframe.grid(sticky="nsew")
+    popuplabel.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+    closebutton.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
     window2.protocol("WM_DELETE_WINDOW", popupdestroy)
     with open('dummy.txt', 'r', encoding="utf_16_be") as f:
         source = f.read()
         for key in values.keys():
             source = source.replace(key, values[key])
-    with open('mission.txt', 'w', encoding="utf_16_be") as f:
+    with open('MISSION.txt', 'w', encoding="utf_16_be") as f:
         f.write(source)
+    p = Popen(["EDF Tools.exe", "MISSION.txt"], stdin=PIPE, shell=True)
+    p.communicate(input=b'\n')
 
 def popupdestroy():
     confirm['state'] = "normal"
